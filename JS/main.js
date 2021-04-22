06c4116f28f4403f6c220558ea9a26647b5a9686
MauroLaBarbera <you@example.com>
2021-04-22 17:35:20 +0200

    method on click, css fix

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+/**
+ * VUE SLIDER
+ */
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        photos: [
+            './img/img-1.jpg',
+            './img/img-2.jpg',
+            './img/img-3.jpg',
+            './img/img-4.jpg',
+        ],
+        indexPhotos: 0,
+    },
+    methods: {
+        prevPhoto() {
+            this.indexPhotos--;
+
+            if (this.indexPhotos < 0 ) {
+                this.indexPhotos = this.photos.length - 1;
+            }
+        },
+
+        nextPhoto() {
+            this.indexPhotos++;
+
+            if (this.indexPhotos > (this.photos.length - 1) ) {
+                this.indexPhotos = 0;
+            } 
+        }
+    }
+});
